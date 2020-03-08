dbbe188342ad92b7a2ceae259ecddbfef39f2f0e
Khalid T <khalid.tissire@gmail.com>
2020-03-08 14:55:31 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20200308145043) do
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
     t.float "balance", default: 0.0, null: false
   end
+
 end
